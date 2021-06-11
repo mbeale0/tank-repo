bb72b0f52448d7b33a6c36522c3458b10d4ad659
TrueRealPlayer <74131924+TrueRealPlayer@users.noreply.github.com>
2021-06-11 09:40:57 +0300

    fixed the issue when player starts the color is not set. Also changed the tank prefab name as a player

--- Assets/Scripts/Managers/MyPlayer.cs
+++ Assets/Scripts/Managers/MyPlayer.cs
@@ -6,7 +6,16 @@ namespace Managers
 {
     public class MyPlayer : NetworkBehaviour
     {
+        NetworkConnection cachedNetworkConnection;
         private Color _teamColor = new Color();
+
+
+        private void Start()
+        {
+            cachedNetworkConnection = connectionToClient;
+            NetworkServer.RemovePlayerForConnection(connectionToClient, false);
+            NetworkServer.AddPlayerForConnection(cachedNetworkConnection, gameObject);
+        }
 
         public Color GetTeamColor()
         {
@@ -18,6 +27,5 @@ namespace Managers
         {
             _teamColor = newTeamColor;
         }
-    
     }
 }
