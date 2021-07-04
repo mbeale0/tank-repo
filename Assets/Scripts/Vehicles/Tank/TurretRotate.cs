061f39863609dccde5ca381965e908058d39e76c
TrueRealPlayer <74131924+TrueRealPlayer@users.noreply.github.com>
2021-07-04 10:40:53 +0300

    made the tank barrel to move up and down
    there is a bug that it goes minus x further than it should, i dont know why ive to figure this out

--- Assets/Scripts/Vehicles/Tank/TurretRotate.cs
+++ Assets/Scripts/Vehicles/Tank/TurretRotate.cs
@@ -6,7 +6,8 @@ namespace Complete
     public class TurretRotate : MonoBehaviour
     {
         [SerializeField] GameObject turret;
-   
+        [SerializeField] GameObject barrel;
+
         public void Rotate()
         {
             if (Input.GetKey("e"))
@@ -33,7 +34,18 @@ namespace Complete
             {
                 AkSoundEngine.PostEvent("Stop_Tank_Rotation", gameObject);
             }
-            
+            if (Input.GetKey("z")) 
+            {
+                if (barrel.transform.rotation.x >= 0) return;
+                    barrel.transform.Rotate(new Vector3(1f, 0, 0f));
+            }
+            if (Input.GetKey("x"))
+            {
+                if (barrel.transform.rotation.x <= -35) return;
+                barrel.transform.Rotate(new Vector3(-1f, 0, 0f));
+            }
+
         }
+    
     }
 }
