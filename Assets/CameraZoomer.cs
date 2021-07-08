1b02bba177c06ad00b834c20f0f863dca6600275
Mason Beale <mbeale@cedarville.edu>
2021-07-08 16:29:11 -0400

    Started testinf camera

---
+++ Assets/CameraZoomer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoomer : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    private void Start()
+    {
+        //rigidBody = gameObject.GetComponentInParent<Rigidbody>();
+    }
+    void Update()
+    {
+        //Vector3 velocity = rigidBody.velocity;
+        //Debug.Log(velocity);
+    }
+}
