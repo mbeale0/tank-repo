b06c5bc6ee994c0ec4e249fbb26211288a77f551
Mason Beale <mbeale@cedarville.edu>
2021-07-07 23:24:43 -0400

    added ammo limiting system and fixed some bugs

--- Assets/Scripts/Vehicles/Tank/TurretRotate.cs
+++ Assets/Scripts/Vehicles/Tank/TurretRotate.cs
@@ -43,46 +43,49 @@ namespace Complete
             {
                 //AkSoundEngine.PostEvent("Stop_Tank_Rotation", gameObject);
             }
-            var barrelVectorRotation = barrel.transform.localRotation.eulerAngles;
-            var smoothTime = 1f;
-            if (Input.GetKey("z"))
+            if(barrel != null)
             {
-                rotateUp = false;
-                hasFired = false;
-            }
-            if (Input.GetKey("x"))
-            {
-                rotateUp = true;
-                hasFired = false;
-            }
-            
-            if (rotateUp)
-            {
-                barrelVectorRotation.x = -35;
+                var barrelVectorRotation = barrel.transform.localRotation.eulerAngles;
+                var smoothTime = 1f;
+                if (Input.GetKey("z"))
+                {
+                    rotateUp = false;
+                    hasFired = false;
+                }
+                if (Input.GetKey("x"))
+                {
+                    rotateUp = true;
+                    hasFired = false;
+                }
+
+                if (rotateUp)
+                {
+                    barrelVectorRotation.x = -35;
+                    //Actually doing the rotation and making it smooth
+                    barrel.transform.localRotation = Quaternion.Slerp(barrel.transform.localRotation, barrelQuaternionRotation, smoothTime * Time.deltaTime);
+                }
+                else if (!rotateUp)
+                {
+                    barrelVectorRotation.x = 0;
+                    //Actually doing the rotation and making it smooth
+                    barrel.transform.localRotation = Quaternion.Slerp(barrel.transform.localRotation, barrelQuaternionRotation, smoothTime * Time.deltaTime);
+                }
+
+                //Clamping the value so it will be between -35 and 0 
+                //barrelVectorRotation.x = Mathf.Clamp(barrelVectorRotation.x, -35, 0);
+
                 //Actually doing the rotation and making it smooth
-                barrel.transform.localRotation = Quaternion.Slerp(barrel.transform.localRotation, barrelQuaternionRotation, smoothTime * Time.deltaTime);
-            }
-            else if (!rotateUp)
-            {
-                barrelVectorRotation.x = 0;
-                //Actually doing the rotation and making it smooth
-                barrel.transform.localRotation = Quaternion.Slerp(barrel.transform.localRotation, barrelQuaternionRotation, smoothTime * Time.deltaTime);
-            }
-
-            //Clamping the value so it will be between -35 and 0 
-            //barrelVectorRotation.x = Mathf.Clamp(barrelVectorRotation.x, -35, 0);
-
-            //Actually doing the rotation and making it smooth
-            barrelQuaternionRotation = Quaternion.Euler(barrelVectorRotation);
+                barrelQuaternionRotation = Quaternion.Euler(barrelVectorRotation);
 
 
-            // eulerangle for barrel starts at 360, and the negative 
-            // rotation is the same as subtracting the rotation from 360
-            if (Mathf.Round(barrel.transform.eulerAngles.x) == (360 + barrelVectorRotation.x) && !hasFired)
-            {
-                hasFired = true;
-                firingScript.CmdFire();
-                
+                // eulerangle for barrel starts at 360, and the negative 
+                // rotation is the same as subtracting the rotation from 360
+                if (Mathf.Round(barrel.transform.eulerAngles.x) == (360 + barrelVectorRotation.x) && !hasFired)
+                {
+                    hasFired = true;
+                    firingScript.CmdFire();
+
+                }
             }
         }
 
